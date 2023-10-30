37351944cda96fb13658f9556b8017db2aaf3a49
Greeshma <pujarigreeshma@gmail.com>
2023-10-30 12:06:09 +0530

    second commit

--- demopush/src/package1/Welcome.java
+++ demopush/src/package1/Welcome.java
@@ -3,7 +3,8 @@ package package1;
 public class Welcome {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to gprec");// TODO Auto-generated method stub
+		System.out.println("Welcome to gprec");
+		System.out.println("hello");// TODO Auto-generated method stub
 
 	}
 
